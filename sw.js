9608d747c6125cbd6ed13dfbf1c03c2f24b29401
myhmmt <muhammetgnc461@gmail.com>
2025-09-14 16:20:36 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,39 +1,30 @@
+// Basit offline cache
 const CACHE = 'gg-terminal-v1';
 const ASSETS = [
   './',
   './index.html',
+  './app.js',
   './manifest.json',
-  './sw.js',
   './icon-192.png',
   './icon-512.png',
   './beep.ogg',
   './error.ogg'
 ];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+self.addEventListener('install', (e)=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
   self.skipWaiting();
 });
-
-self.addEventListener('activate', e => {
+self.addEventListener('activate', (e)=>{
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
   );
   self.clients.claim();
 });
-
-self.addEventListener('fetch', e => {
+self.addEventListener('fetch', (e)=>{
   const req = e.request;
+  // Önce cache, yoksa ağ; ağ da yoksa cachete ne varsa
   e.respondWith(
-    caches.match(req).then(cached => {
-      if (cached) return cached;
-      return fetch(req).then(res => {
-        const copy = res.clone();
-        caches.open(CACHE).then(c => c.put(req, copy));
-        return res;
-      }).catch(() => caches.match('./index.html'));
-    })
+    caches.match(req).then(res=> res || fetch(req).catch(()=>caches.match('./index.html')))
   );
 });
