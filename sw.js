01e78222673e03dcd097f0276cb0e95b6462e50d
myhmmt <muhammetgnc461@gmail.com>
2025-09-15 11:00:00 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,41 +1,39 @@
-const CACHE = "gg-mobil-v5";
-
-// Önbelleğe alınacak çekirdek dosyalar
-const CORE = [
-  "index.html",
-  "app.js",
-  "manifest.json",
-  "icon-192.png",
-  "icon-512.png",
-  "beep.ogg",
-  "error.ogg"
+// Basit PWA Service Worker – offline çalışma
+const CACHE = 'gg-terminal-v6';
+const ASSETS = [
+  './',
+  './index.html',
+  './app.js',
+  './manifest.json',
+  './beep.ogg',
+  './error.ogg',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
-self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(CORE)));
-  self.skipWaiting();
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
 });
 
-self.addEventListener("activate", (e) => {
+self.addEventListener('activate', e=>{
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))))
   self.clients.claim();
 });
 
-// Stale-while-revalidate
-self.addEventListener("fetch", (e) => {
+self.addEventListener('fetch', e=>{
   const req = e.request;
+  // önce cache, sonra ağ
   e.respondWith(
-    caches.match(req).then(cached => {
-      const fetchPromise = fetch(req).then(res => {
-        const copy = res.clone();
-        caches.open(CACHE).then(c => c.put(req, copy));
-        return res;
-      }).catch(()=>cached || Promise.reject("offline"));
-      return cached || fetchPromise;
-    })
+    caches.match(req).then(res=> res || fetch(req).then(net=>{
+      // GET ve aynı origin ise cache’e koy
+      try{
+        if(req.method==='GET' && new URL(req.url).origin===location.origin){
+          const clone = net.clone();
+          caches.open(CACHE).then(c=>c.put(req, clone));
+        }
+      }catch{}
+      return net;
+    }).catch(()=>caches.match('./index.html')))
   );
 });
