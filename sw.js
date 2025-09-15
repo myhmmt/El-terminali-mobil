0c0c7398aa375951130c5541f0877d8d39fc00a8
myhmmt <muhammetgnc461@gmail.com>
2025-09-16 02:21:45 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,41 @@
-// Offline PWA Service Worker (v7)
-const CACHE = 'gg-terminal-v7';
-const ASSETS = [
-  './','./index.html','./app.js','./manifest.json',
-  './beep.ogg','./error.ogg','./icon-192.png','./icon-512.png'
+const CACHE = "gg-mobil-v5";
+
+// Önbelleğe alınacak çekirdek dosyalar
+const CORE = [
+  "index.html",
+  "app.js",
+  "manifest.json",
+  "icon-192.png",
+  "icon-512.png",
+  "beep.ogg",
+  "error.ogg"
 ];
 
-self.addEventListener('install', e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(CORE)));
+  self.skipWaiting();
 });
-self.addEventListener('activate', e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
   self.clients.claim();
 });
-self.addEventListener('fetch', e=>{
-  const req=e.request;
+
+// Stale-while-revalidate
+self.addEventListener("fetch", (e) => {
+  const req = e.request;
   e.respondWith(
-    caches.match(req).then(res=>res||fetch(req).then(net=>{
-      try{
-        if(req.method==='GET' && new URL(req.url).origin===location.origin){
-          const clone=net.clone(); caches.open(CACHE).then(c=>c.put(req,clone));
-        }
-      }catch{}
-      return net;
-    }).catch(()=>caches.match('./index.html')))
+    caches.match(req).then(cached => {
+      const fetchPromise = fetch(req).then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(req, copy));
+        return res;
+      }).catch(()=>cached || Promise.reject("offline"));
+      return cached || fetchPromise;
+    })
   );
 });
