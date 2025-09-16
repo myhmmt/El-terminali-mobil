9dc93652b69c1be8a952e115829502f7c24d7dd9
myhmmt <muhammetgnc461@gmail.com>
2025-09-16 03:18:13 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,28 +1,25 @@
-const CACHE = "gg-mobil-v5";
-
-// Önbelleğe alınacak çekirdek dosyalar
-const CORE = [
-  "index.html",
-  "app.js",
-  "manifest.json",
-  "icon-192.png",
-  "icon-512.png",
-  "beep.ogg",
-  "error.ogg"
+const CACHE = "ggmt-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./app.js",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./beep.ogg",
+  "./error.ogg",
+  "./accepted.ogg",
+  "./unkown.ogg"
 ];
 
 self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(CORE)));
-  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(()=> self.skipWaiting())
+  );
 });
 
 self.addEventListener("activate", (e) => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
+  e.waitUntil(self.clients.claim());
 });
 
 // Stale-while-revalidate
