0785ff7f36cf535a4d8429b5b20ec518dd9f7465
myhmmt <muhammetgnc461@gmail.com>
2025-09-16 04:53:15 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,38 +1,21 @@
 const CACHE = "ggmt-v2";
 const ASSETS = [
-  "./",
-  "./index.html",
-  "./app.js",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png",
-  "./beep.ogg",
-  "./error.ogg",
-  "./accepted.ogg",
-  "./unkown.ogg"
+  "./","./index.html","./app.js","./manifest.json",
+  "./icon-192.png","./icon-512.png",
+  "./beep.ogg","./error.ogg","./accepted.ogg","./unkown.ogg"
 ];
-
-self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(()=> self.skipWaiting())
-  );
+self.addEventListener("install", e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
 });
-
-self.addEventListener("activate", (e) => {
-  e.waitUntil(self.clients.claim());
-});
-
-// Stale-while-revalidate
-self.addEventListener("fetch", (e) => {
-  const req = e.request;
+self.addEventListener("activate", e=>{ e.waitUntil(self.clients.claim()); });
+self.addEventListener("fetch", e=>{
+  const req=e.request;
   e.respondWith(
-    caches.match(req).then(cached => {
-      const fetchPromise = fetch(req).then(res => {
-        const copy = res.clone();
-        caches.open(CACHE).then(c => c.put(req, copy));
-        return res;
-      }).catch(()=>cached || Promise.reject("offline"));
-      return cached || fetchPromise;
+    caches.match(req).then(cached=>{
+      const fetchPromise=fetch(req).then(res=>{
+        caches.open(CACHE).then(c=>c.put(req,res.clone())); return res;
+      }).catch(()=>cached||Promise.reject("offline"));
+      return cached||fetchPromise;
     })
   );
 });
