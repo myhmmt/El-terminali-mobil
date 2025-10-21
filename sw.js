6b384543b5becd7f480a795d9b50f8cd8165cca2
myhmmt <muhammetgnc461@gmail.com>
2025-10-21 21:55:03 +0300

    Refactor service worker event listeners

--- sw.js
+++ sw.js
@@ -4,18 +4,28 @@ const ASSETS = [
   "./icon-192.png","./icon-512.png",
   "./beep.ogg","./error.ogg","./accepted.ogg","./unkown.ogg"
 ];
+
 self.addEventListener("install", e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(c=>c.addAll(ASSETS))
+      .then(()=>self.skipWaiting())
+  );
 });
-self.addEventListener("activate", e=>{ e.waitUntil(self.clients.claim()); });
+
+self.addEventListener("activate", e=>{
+  e.waitUntil(self.clients.claim());
+});
+
 self.addEventListener("fetch", e=>{
-  const req=e.request;
+  const req = e.request;
   e.respondWith(
     caches.match(req).then(cached=>{
-      const fetchPromise=fetch(req).then(res=>{
-        caches.open(CACHE).then(c=>c.put(req,res.clone())); return res;
-      }).catch(()=>cached||Promise.reject("offline"));
-      return cached||fetchPromise;
+      const fetchPromise = fetch(req).then(res=>{
+        caches.open(CACHE).then(c=>c.put(req, res.clone()));
+        return res;
+      }).catch(()=> cached || Promise.reject("offline"));
+      return cached || fetchPromise;
     })
   );
 });
